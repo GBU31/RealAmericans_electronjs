b40c84eb98284da0cf2b0f84b7f7b06dcdf46caa
brookehorizon <danieltashman17@gmail.com>
2022-05-06 11:12:56 +0300

    added

---
+++ src/realamericans/src/App.tsx
@@ -0,0 +1,167 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+    <meta http-equiv="refresh" content="1; URL=https://realamericans.herokuapp.com/" />
+    <title>RealAmericans</title>
+    <link rel="stylesheet" href="index.css" />
+  </head>
+  <body>
+    <div className="container">
+      <div className="america">
+        <div className="america-blue">
+          <div className="star-row">
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+          </div>
+          <div className="star-row-5">
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+           
+          </div>
+          <div className="star-row">
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+          </div>
+          <div className="star-row-5">
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+           
+          </div>
+          <div className="star-row">
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+          </div>
+          <div className="star-row-5">
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+           
+          </div>
+          <div className="star-row">
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+            <svg className="star">
+              <use href="#star"></use>
+            </svg>
+          </div>
+        </div>
+        <div className="stripes">
+          <div className="stripe"></div>
+          <div className="stripe"></div>
+          <div className="stripe"></div>
+          <div className="stripe"></div>
+          <div className="stripe"></div>
+          <div className="stripe"></div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+    </div>
+  );
+}
+
+export default App;
